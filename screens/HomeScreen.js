7efabb912baecfc0b4445031bccebd31935481f7
snypech <sneyder.yepes@gmail.com>
2021-11-17 10:18:02 -0500

    initial commit

---
+++ screens/HomeScreen.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const HomeScreen = () => {
+    return (
+        <View>
+            <Text>This is my home screen</Text>
+        </View>
+    )
+}
+
+export default HomeScreen
+
+const styles = StyleSheet.create({})
